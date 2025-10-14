6e178fd3793dda6196d792d5142a44bb031c092f
Remzi Balakisiyev <97171141+RemziBalakishiyev@users.noreply.github.com>
2025-10-14 21:43:41 +0400

    Add health checks and pagination to product endpoints
    Updated `Catalog.Api` to include health check packages and modified product retrieval to support pagination. Configured Docker services for `catalog.api` and added new projects for `Basket` and `Basket.API`. Created necessary configuration files for logging and application settings. Initialized `Basket.API` with a Dockerfile and launch settings for development.

--- src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
@@ -1,14 +1,15 @@
 ﻿
+using Marten.Pagination;
+
 namespace Catalog.Api.Products.GetProducts;
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 internal class GetProductsQueryHandler(IDocumentSession session,ILogger<GetProductsResult> logger) 
     : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Handling GetProductsQuery");
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber.Value,query.PageSize.Value,cancellationToken);
         return new GetProductsResult(products);
     }
 }
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,6 +1,8 @@
 using BuildingBlocks.Behaviours;
 using BuildingBlocks.Exceptions.Handler;
 using Catalog.Api.Data;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,9 +26,19 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection")!);
 
 
 var app = builder.Build();
+
 app.UseExceptionHandler(options => { });
+
 app.MapCarter();
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
 app.Run();
